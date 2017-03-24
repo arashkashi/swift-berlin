e91b9e9a09fa256e597d3ce54a87ba67488b557f
choose <arash.kashi@sharecare.com>
2017-03-24 15:35:54 +0100

    Initial Commit

---
+++ SwiftBerlin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftBerlin
+//
+//  Created by Arash Kashi on 3/24/17.
+//  Copyright © 2017 Arash Kashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
